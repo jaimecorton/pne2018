f1ad5718af61effa2b57026f981b9eb4c3dfbd4b
jaimecorton <37112972+jaimecorton@users.noreply.github.com>
2018-03-06 15:46:30 +0100

    Add files via upload

---
+++ Programación/server_web.py
@@ -0,0 +1,65 @@
+# A basic web server using sockets
+
+
+import socket
+
+PORT = 8097
+MAX_OPEN_REQUESTS = 5
+
+
+
+
+
+
+
+#f=open("C:\\Users\\Jaime\\Desktop\\web.html","r")   double\ for a good read of the file path
+
+f=open("C:\\Users\\Jaime\\Desktop\\web.html","r")
+
+
+
+
+
+
+def process_client(clientsocket):
+    print(clientsocket)
+    print(clientsocket.recv(1024))
+	
+    web_contents = f.read()
+    web_headers = "HTTP/1.1 200"
+    web_headers += "\n" + "Content-Type: text/html"
+    web_headers += "\n" + "Content-Length: %i" % len(str.encode(web_contents))
+    clientsocket.send(str.encode(web_headers + "\n\n" + web_contents))
+	    
+    clientsocket.close()
+
+	
+
+    
+	    
+
+
+# create an INET, STREAMing socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# bind the socket to a public host, and a well-known port
+hostname = socket.gethostname()
+# Let's use better the local interface name
+hostname = "10.0.28.58" #my computer IP
+try:
+    serversocket.bind((hostname, PORT))
+    # become a server socket
+    # MAX_OPEN_REQUESTS connect requests before refusing outside connections
+    serversocket.listen(MAX_OPEN_REQUESTS)
+
+    while True:
+        # accept connections from outside
+        print ("Waiting for connections at %s %i" % (hostname, PORT))
+        (clientsocket, address) = serversocket.accept()
+        # now do something with the clientsocket
+        # in this case, we'll pretend this is a non threaded server
+        process_client(clientsocket)
+
+except socket.error as ex:
+
+    print("Problemas using port %i. Do you have permission?" % PORT)
+    print(ex) 
